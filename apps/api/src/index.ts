a40c826b3e8ef9c24fc323ef9e11797ce93bfc46
danilobml <d.lima@gp-joule.de>
2025-05-10 21:08:03 +0200

    setup monorepo

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,13 +1,13 @@
-import express, { json } from 'express';
-import cors from 'cors';
+import express, { json } from 'express'
+import cors from 'cors'
 
-const app = express();
+const app = express()
 
-app.use(express.json());
-app.use(cors());
+app.use(express.json())
+app.use(cors())
 
-app.get("/", (req, res) => {
-    res.send("Hello world!")
+app.get('/', (req, res) => {
+  res.send('Hello world!')
 })
 
 app.listen(8080, () => console.log(`Listening on port 8080`))
